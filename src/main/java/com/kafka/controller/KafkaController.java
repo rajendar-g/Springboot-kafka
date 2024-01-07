5035096a5fbbd31800144dafadd110e93e86fa59
rajendar-g <ctschat@gmail.com>
2024-01-07 10:36:34 +0530

    first commit

---
+++ src/main/java/com/kafka/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package com.kafka.controller;
+
+import com.kafka.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+    @Autowired
+    private MessageProducer messageProducer;
+
+
+    @GetMapping("/")
+    public String getMessage(@RequestParam("message") String message) {
+
+        return "Message sent: " + message;
+    }
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+}
